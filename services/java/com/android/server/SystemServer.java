7f7d76958bc88f3adf28ab3a3f17717639ab4738
minaripenguin <minaripenguin@users.noreply.github.com>
2024-03-26 09:27:10 +0800

    services: Introduce RisingServicesStarter
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -289,6 +289,8 @@ import com.android.server.wm.WindowManagerService;
 
 import dalvik.system.VMRuntime;
 
+import org.rising.server.RisingServicesStarter;
+
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.IOException;
@@ -3013,6 +3015,9 @@ public final class SystemServer implements Dumpable {
         t.traceBegin("HealthConnectManagerService");
         mSystemServiceManager.startService(HEALTHCONNECT_MANAGER_SERVICE_CLASS);
         t.traceEnd();
+
+        RisingServicesStarter risingServiceStarter = new RisingServicesStarter(mSystemServiceManager);
+        risingServiceStarter.startAllServices();
 
         if (mPackageManager.hasSystemFeature(PackageManager.FEATURE_DEVICE_LOCK)) {
             t.traceBegin("DeviceLockService");
