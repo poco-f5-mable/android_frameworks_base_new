cf27ce0bb432423250ecae9fee0885c1ccc7df67
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-23 15:48:36 +0800

    Observe status bar bg chip changes
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/theme/RisingSettingsConstants.java
+++ packages/SystemUI/src/com/android/systemui/theme/RisingSettingsConstants.java
@@ -20,8 +20,10 @@ import android.provider.Settings;
 public class RisingSettingsConstants {
     public static final String CLOCK_STYLE = "clock_style";
     public static final String VOLUME_STYLE = "custom_volume_styles";
+    public static final String STATUSBAR_CLOCK_CHIP = Settings.System.STATUSBAR_CLOCK_CHIP;
 
     public static final String[] SYSTEM_SETTINGS_KEYS = {
+        STATUSBAR_CLOCK_CHIP
     };
     
     public static final String[] SECURE_SETTINGS_KEYS = {
