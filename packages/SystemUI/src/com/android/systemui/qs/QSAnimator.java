50055f9a65247bffc71c7178a52f55349c546430
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-26 22:55:34 +0800

    QSAnimator: Remove TunerService usage
    * we can safely remove this since we re-evaluate the theme when enabling a11 overlay
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSAnimator.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSAnimator.java
@@ -36,7 +36,6 @@ import com.android.systemui.qs.QSPanel.QSTileLayout;
 import com.android.systemui.qs.TouchAnimator.Builder;
 import com.android.systemui.qs.dagger.QSScope;
 import com.android.systemui.qs.tileimpl.HeightOverrideable;
-import com.android.systemui.tuner.TunerService;
 import com.android.systemui.util.concurrency.DelayableExecutor;
 
 import java.util.ArrayList;
@@ -61,12 +60,9 @@ import javax.inject.Inject;
 @QSScope
 public class QSAnimator implements QSHost.Callback, PagedTileLayout.PageListener,
         TouchAnimator.Listener, OnLayoutChangeListener,
-        OnAttachStateChangeListener, TunerService.Tunable {
+        OnAttachStateChangeListener {
 
     private static final String TAG = "QSAnimator";
-
-    public static final String QS_TILE_UI_STYLE =
-            "system:" + Settings.System.QS_TILE_UI_STYLE;
 
     private static final int ANIMATORS_UPDATE_DELAY_MS = 100;
     private static final float EXPANDED_TILE_DELAY = .86f;
@@ -139,7 +135,6 @@ public class QSAnimator implements QSHost.Callback, PagedTileLayout.PageListener
     private float mLastPosition;
     private final QSHost mHost;
     private final DelayableExecutor mExecutor;
-    private final TunerService mTunerService;
     private boolean mShowCollapsedOnKeyguard;
     private int mQQSTop;
     private boolean isA11Style;
@@ -151,7 +146,7 @@ public class QSAnimator implements QSHost.Callback, PagedTileLayout.PageListener
     public QSAnimator(@RootView View rootView, QuickQSPanel quickPanel,
             QSPanelController qsPanelController,
             QuickQSPanelController quickQSPanelController, QSHost qsTileHost,
-            @Main DelayableExecutor executor, TunerService tunerService,
+            @Main DelayableExecutor executor,
             QSExpansionPathInterpolator qsExpansionPathInterpolator) {
         mQsRootView = rootView;
         mQuickQsPanel = quickPanel;
@@ -159,7 +154,6 @@ public class QSAnimator implements QSHost.Callback, PagedTileLayout.PageListener
         mQuickQSPanelController = quickQSPanelController;
         mHost = qsTileHost;
         mExecutor = executor;
-        mTunerService = tunerService;
         mQSExpansionPathInterpolator = qsExpansionPathInterpolator;
         mHost.addCallback(this);
         mQsPanelController.addOnAttachStateChangeListener(this);
@@ -217,7 +211,6 @@ public class QSAnimator implements QSHost.Callback, PagedTileLayout.PageListener
 
     @Override
     public void onViewAttachedToWindow(@NonNull View view) {
-        mTunerService.addTunable(this, QS_TILE_UI_STYLE);
         updateAnimators();
         setCurrentPosition();
     }
@@ -225,19 +218,6 @@ public class QSAnimator implements QSHost.Callback, PagedTileLayout.PageListener
     @Override
     public void onViewDetachedFromWindow(@NonNull View v) {
         mHost.removeCallback(this);
-        mTunerService.removeTunable(this);
-    }
-
-    @Override
-    public void onTuningChanged(String key, String newValue) {
-        switch (key) {
-            case QS_TILE_UI_STYLE:
-                isA11Style =
-                     TunerService.parseInteger(newValue, 0) != 0;
-                break;
-            default:
-                break;
-         }
     }
 
     private void addNonFirstPageAnimators(int page) {
@@ -295,6 +275,7 @@ public class QSAnimator implements QSHost.Callback, PagedTileLayout.PageListener
     }
 
     private void updateAnimators() {
+        isA11Style = com.android.internal.util.systemui.qs.QSLayoutUtils.getQsUiStyle(mHost.getContext()) != 0;
         mNeedsAnimatorUpdate = false;
         TouchAnimator.Builder firstPageBuilder = new Builder();
         TouchAnimator.Builder translationYBuilder = new Builder();
